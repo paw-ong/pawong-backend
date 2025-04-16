122c1c681c8aea2cf062db2e49318cfc66522cdf
Yoo Hyunseok <brianyhs@naver.com>
2025-04-16 14:32:04 +0900

    Initial commit

---
+++ src/main/java/kr/co/pawong/pwbe/PawongBackendApplication.java
@@ -0,0 +1,13 @@
+package kr.co.pawong.pwbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawongBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawongBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/pawong/pwbe/PawongBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.pawong.pwbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawongBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
