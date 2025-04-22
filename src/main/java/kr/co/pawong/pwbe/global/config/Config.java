6d214fcaf78141ef9ab5dc4e8566f03553cafaa4
yongje <dldydwp9024@gmail.com>
2025-04-22 19:33:40 +0900

    [feat] config 추가

---
+++ src/main/java/kr/co/pawong/pwbe/global/config/Config.java
@@ -0,0 +1,30 @@
+package kr.co.pawong.pwbe.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Arrays.asList(
+                MediaType.APPLICATION_JSON,
+                MediaType.valueOf("application/json;charset=UTF-8"),
+                MediaType.valueOf("text/json;charset=UTF-8")
+        ));
+
+        restTemplate.setMessageConverters(Collections.singletonList(converter));
+        return restTemplate;
+    }
+}
+
