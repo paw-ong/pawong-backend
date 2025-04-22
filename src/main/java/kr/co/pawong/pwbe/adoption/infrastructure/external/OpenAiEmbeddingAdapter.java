a0fc94c96e86718b887791a88a0a985abf01f775
Dingwoonee <slm0523@naver.com>
2025-04-22 17:20:46 +0900

    [feat] OpenAi Embedding 코드 개선

--- src/main/java/kr/co/pawong/pwbe/adoption/infrastructure/external/OpenAiEmbeddingAdapter.java
+++ src/main/java/kr/co/pawong/pwbe/adoption/infrastructure/external/OpenAiEmbeddingAdapter.java
@@ -2,42 +2,19 @@ package kr.co.pawong.pwbe.adoption.infrastructure.external;
 
 import kr.co.pawong.pwbe.adoption.application.service.port.EmbeddingProcessorPort;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.document.MetadataMode;
-import org.springframework.ai.embedding.EmbeddingResponse;
-import org.springframework.ai.openai.OpenAiEmbeddingModel;
-import org.springframework.ai.openai.OpenAiEmbeddingOptions;
-import org.springframework.ai.openai.api.OpenAiApi;
-import org.springframework.ai.retry.RetryUtils;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @Qualifier("OPENAI_EMBEDDING")
 @RequiredArgsConstructor
 public class OpenAiEmbeddingAdapter implements EmbeddingProcessorPort {
 
-    private final OpenAiApi openAiApi;
-
-    @Value("${spring.ai.openai.embedding-model-name}")
-    private String embeddingModelName;
+    private final EmbeddingModel embeddingModel;
 
     @Override
     public float[] embed(String completion) {
-        OpenAiEmbeddingModel openAiEmbeddingModel = new OpenAiEmbeddingModel(
-                this.openAiApi,
-                MetadataMode.EMBED,
-                OpenAiEmbeddingOptions.builder()
-                        .model(embeddingModelName)
-                        .user("user-6")
-                        .build(),
-                RetryUtils.DEFAULT_RETRY_TEMPLATE);
-
-        EmbeddingResponse embeddingResponse = openAiEmbeddingModel
-                .embedForResponse(List.of(completion));
-
-        return embeddingResponse.getResults().getFirst().getOutput();
+        return embeddingModel.embed(completion);
     }
 }
