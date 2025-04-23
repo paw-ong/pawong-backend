0a88f253cdeca10e8837e91e1d00c352a21cc1ae
Dingwoonee <slm0523@naver.com>
2025-04-23 11:18:34 +0900

    [docs] AdoptionAiServiceImpl에 주석 추가

--- src/main/java/kr/co/pawong/pwbe/adoption/application/service/AdoptionAiServiceImpl.java
+++ src/main/java/kr/co/pawong/pwbe/adoption/application/service/AdoptionAiServiceImpl.java
@@ -5,6 +5,9 @@ import kr.co.pawong.pwbe.adoption.application.service.port.PromptProcessorPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * @exception: null이나 빈 문자열, 공백이 입력된 경우 IllegalArgumentException을 던집니다.
+ */
 @Service
 @RequiredArgsConstructor
 public class AdoptionAiServiceImpl implements AdoptionAiService {
@@ -12,18 +15,21 @@ public class AdoptionAiServiceImpl implements AdoptionAiService {
     private final EmbeddingProcessorPort embeddingPort;
     private final PromptProcessorPort promptPort;
 
+    // 문장 정제하는 함수
     @Override
     public String refineSearchCondition(String searchTerm) {
         validateNotBlank(searchTerm);
         return promptPort.refineByPrompt(searchTerm);
     }
 
+    // 문장 입베딩하는 함수
     @Override
     public float[] embed(String completion) {
         validateNotBlank(completion);
         return embeddingPort.embed(completion);
     }
 
+    // 입력값 검증하는 함수
     private void validateNotBlank(String input) {
         if (input == null || input.isBlank()) {
             throw new IllegalArgumentException("Input cannot be null or blank.");
