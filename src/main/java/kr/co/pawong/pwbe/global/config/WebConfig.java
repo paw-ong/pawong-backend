6e302e69efaff8ca62b5600c7e5de0b03f8f6343
Yoo Hyunseok <brianyhs@naver.com>
2025-04-23 17:49:28 +0900

    fix: UpKindCd 문자열 인식
    todo: all shards failed 원인 파악

--- src/main/java/kr/co/pawong/pwbe/global/config/WebConfig.java
+++ src/main/java/kr/co/pawong/pwbe/global/config/WebConfig.java
@@ -1,12 +1,15 @@
-package kr.co.pawong.pwbe.config;
+package kr.co.pawong.pwbe.global.config;
 
-import kr.co.pawong.pwbe.config.converter.StringToUpKindConverter;
+import kr.co.pawong.pwbe.global.config.converter.StringToUpKindConverter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final StringToUpKindConverter stringToUpKindConverter;
 
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToUpKindConverter());
--- src/main/java/kr/co/pawong/pwbe/global/config/converter/StringToUpKindConverter.java
+++ src/main/java/kr/co/pawong/pwbe/global/config/converter/StringToUpKindConverter.java
@@ -1,11 +1,13 @@
-package kr.co.pawong.pwbe.config.converter;
+package kr.co.pawong.pwbe.global.config.converter;
 
 import kr.co.pawong.pwbe.adoption.enums.UpKindCd;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StringToUpKindConverter implements Converter<String, UpKindCd> {
     @Override
     public UpKindCd convert(String source) {
-        return UpKindCd.valueOf(source);
+        return UpKindCd.fromValue(source);
     }
 }
