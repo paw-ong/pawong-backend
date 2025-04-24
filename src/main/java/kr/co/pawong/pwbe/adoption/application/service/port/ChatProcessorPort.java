c2ab37487038a7e2ed5affb6922f89435de0eb98
Dingwoonee <slm0523@naver.com>
2025-04-24 11:11:39 +0900

    [chore] Port 주석 추가

--- src/main/java/kr/co/pawong/pwbe/adoption/application/service/port/ChatProcessorPort.java
+++ src/main/java/kr/co/pawong/pwbe/adoption/application/service/port/ChatProcessorPort.java
@@ -1,6 +1,11 @@
 package kr.co.pawong.pwbe.adoption.application.service.port;
 
-public interface PromptProcessorPort {
+public interface ChatProcessorPort {
 
-    String refineByPrompt(String prompt);
+    /**
+     * 동물 특징을 기반으로 문장을 반환합니다.
+     * @param feature - 동물 특징 문자열
+     * @return AI 모델 응답 문자열
+     */
+    String refineByFeature(String feature);
 }
