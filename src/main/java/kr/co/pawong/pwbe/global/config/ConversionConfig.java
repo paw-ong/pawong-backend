08a24bb0e7a7e49dadffd2f93e66f9f642bf849a
Yoo Hyunseok <brianyhs@naver.com>
2025-04-25 23:14:38 +0900

    refactor: WebConfig 클래스명이 역할에 맞도록 구체적으로 네이밍 및 주석 추가

--- src/main/java/kr/co/pawong/pwbe/global/config/ConversionConfig.java
+++ src/main/java/kr/co/pawong/pwbe/global/config/ConversionConfig.java
@@ -6,9 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// upKindCd에 대한 client 요청을 UpKindCd enum 타입으로 변환할 수 있도록 하는 configuration
 @Configuration
 @RequiredArgsConstructor
-public class WebConfig implements WebMvcConfigurer {
+public class ConversionConfig implements WebMvcConfigurer {
     private final StringToUpKindConverter stringToUpKindConverter;
 
     public void addFormatters(FormatterRegistry registry) {
