2d6f79377915d88cceb36f85f9e87cba6eaa4080
Dingwoonee <slm0523@naver.com>
2025-04-26 17:44:36 +0900

    [chore] 주석을 추가함

--- src/main/java/kr/co/pawong/pwbe/adoption/application/service/util/AdoptionAiExecutor.java
+++ src/main/java/kr/co/pawong/pwbe/adoption/application/service/util/AdoptionAiExecutor.java
@@ -21,7 +21,7 @@ public class AdoptionAiExecutor {
      * inputs 의 각 요소에 fn.apply 를 병렬로 수행하고,
      * 모든 작업이 끝난 뒤 결과 리스트를 돌려줍니다.
      * @param inputs - 이 순서와 결과물의 순서가 같습니다.
-     * @return 반환 리스트에서 Optional empty인 항목은 예외가 발생한 항목입니다.
+     * @return 반환 리스트에서 Optional `empty`인 항목은 결과가 `null`이거나 예외가 발생한 항목입니다.
      */
     public <T, R> List<Optional<R>> run(List<T> inputs, Function<T, R> fn) {
         // 각 입력마다 CompletableFuture 생성
