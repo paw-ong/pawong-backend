33be00a7fdc4fb80c9046b75f1d9399049a3652c
Yoo Hyunseok <brianyhs@naver.com>
2025-04-27 21:48:47 +0900

    feat: adoption search api명을 restful하게 변경

--- src/main/java/kr/co/pawong/pwbe/adoption/presentation/controller/AdoptionSearchController.java
+++ src/main/java/kr/co/pawong/pwbe/adoption/presentation/controller/AdoptionSearchController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/adoption")
+@RequestMapping("/api/adoptions")
 @RestController
 @RequiredArgsConstructor
 public class AdoptionSearchController {
