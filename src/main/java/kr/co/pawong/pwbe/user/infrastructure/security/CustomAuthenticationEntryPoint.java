53d5d466c57b348332a4bc418dd103f83d58c822
Sungyeon <csy357@gmail.com>
2025-04-28 10:57:10 +0900

    [feat] security exception handling

--- src/main/java/kr/co/pawong/pwbe/user/infrastructure/security/CustomAuthenticationEntryPoint.java
+++ src/main/java/kr/co/pawong/pwbe/user/infrastructure/security/CustomAuthenticationEntryPoint.java
@@ -1,11 +1,15 @@
 package kr.co.pawong.pwbe.user.infrastructure.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
+import kr.co.pawong.pwbe.user.infrastructure.security.exception.UserNotActiveException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -19,7 +23,17 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
                                  request, HttpServletResponse response, AuthenticationException
                                  authException) throws IOException, ServletException {
         log.warn("Authentication exception: {} {} {}", authException, request, response);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+        String body = new ObjectMapper().writeValueAsString(
+            Map.of(
+                "code", authException instanceof UserNotActiveException
+                    ? "NEED_ADDITIONAL_INFO"
+                    : "UNAUTHORIZED",
+                "message", authException.getMessage()
+            )
+        );
+        response.getWriter().write(body);
     }
 
 }
