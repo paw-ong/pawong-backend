bfa606e19bc2ef9b1d0cdf839c0facdb4f5e6940
Sungyeon <csy357@gmail.com>
2025-04-28 10:38:17 +0900

    [feat] user 정보 조회 API

--- src/main/java/kr/co/pawong/pwbe/user/presentation/controller/dto/response/UserResponse.java
+++ src/main/java/kr/co/pawong/pwbe/user/presentation/controller/dto/response/UserResponse.java
@@ -1,18 +1,35 @@
 package kr.co.pawong.pwbe.user.presentation.controller.dto.response;
 
+import java.time.LocalDate;
 import java.util.List;
+import kr.co.pawong.pwbe.user.application.domain.User;
+import kr.co.pawong.pwbe.user.enums.UserStatus;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponse {
-    private String username;
-    private List<String> roles;
+    private Long userId;
+    private String nickname;
+    private String profileImage;
+    private String region;
+    private String tel;
+    private UserStatus status;
+    private LocalDate createdAt;
 
-    public static UserResponse of(String username, List<String> roles) {
-        return new UserResponse(username, roles);
+    public UserResponse(User user) {
+        this.userId = user.getUserId();
+        this.nickname = user.getNickname();
+        this.profileImage = user.getProfileImage();
+        this.region = user.getRegion();
+        this.tel = user.getTel();
+        this.status = user.getStatus();
+        this.createdAt = user.getCreatedAt();
     }
+
 }
