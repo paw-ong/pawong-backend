6fe691e97f7ccb005eb12cb9a98dcc56d389a8f0
h0725j <guswldpqj1@naver.com>
2025-04-28 15:43:13 +0900

    [refactor] RDB 저장 로직

--- src/main/java/kr/co/pawong/pwbe/adoption/application/service/AdoptionUpdateServiceImpl.java
+++ src/main/java/kr/co/pawong/pwbe/adoption/application/service/AdoptionUpdateServiceImpl.java
@@ -3,18 +3,18 @@ package kr.co.pawong.pwbe.adoption.application.service;
 import java.util.List;
 import kr.co.pawong.pwbe.adoption.application.domain.Adoption;
 import kr.co.pawong.pwbe.adoption.application.domain.AdoptionCreate;
-import kr.co.pawong.pwbe.adoption.application.service.port.AdoptionIndexRepository;
 import kr.co.pawong.pwbe.adoption.application.service.port.AdoptionUpdateRepository;
 import kr.co.pawong.pwbe.adoption.presentation.port.AdoptionUpdateService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AdoptionUpdateServiceImpl implements AdoptionUpdateService {
 
     private final AdoptionUpdateRepository adoptionUpdateRepository;
-    private final AdoptionIndexRepository adoptionIndexRepository;
 
     // AdoptionCreate -> Adoption -> Repo에 전달
     @Override
@@ -24,6 +24,7 @@ public class AdoptionUpdateServiceImpl implements AdoptionUpdateService {
                 .toList();
 
         adoptionUpdateRepository.saveAdoptions(adoptions);
-        adoptionIndexRepository.saveAdoptions(adoptions);
+
+        log.info("{}개의 입양 정보가 변환 및 저장되었습니다.", adoptions.size());
     }
 }
