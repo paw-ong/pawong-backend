6ba7e24c9ddb1b046bdcab06f92999fba80a6070
h0725j <guswldpqj1@naver.com>
2025-04-30 11:45:33 +0900

    [feat] updateEmbeddingDone, updateAiFields 추가

--- src/main/java/kr/co/pawong/pwbe/adoption/application/service/port/AdoptionUpdateRepository.java
+++ src/main/java/kr/co/pawong/pwbe/adoption/application/service/port/AdoptionUpdateRepository.java
@@ -6,4 +6,10 @@ import kr.co.pawong.pwbe.adoption.application.domain.Adoption;
 public interface AdoptionUpdateRepository {
     // 유기동물정보 RDB에 저장
     void saveAdoptions(List<Adoption> adoptions);
+
+    // EmbeddingDone 업데이트
+    void updateEmbeddingDone(List<Adoption> adoptions);
+
+    // Ai 관련 데이터 업데이트
+    void updateAiFields(List<Adoption> adoptions);
 }
