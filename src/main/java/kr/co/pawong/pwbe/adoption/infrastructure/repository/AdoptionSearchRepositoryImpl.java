710808c22255782580bbec8f13ab6125d4001302
김성운 <136048882+DingWoonee@users.noreply.github.com>
2025-05-01 23:41:24 +0900

    [feat] 자동완성 기능을 구현 (#30)
    * [feat] 자동완성에 사용할 document와 response dto 생성
    
    * [feat] 자동완성을 위해 ES 조회
    
    * [fix] ES Dockerfile 수정
    
    * [fix] ES Dockerfile에 icu 추가

--- src/main/java/kr/co/pawong/pwbe/adoption/infrastructure/repository/AdoptionSearchRepositoryImpl.java
+++ src/main/java/kr/co/pawong/pwbe/adoption/infrastructure/repository/AdoptionSearchRepositoryImpl.java
@@ -7,7 +7,9 @@ import kr.co.pawong.pwbe.adoption.application.domain.Adoption;
 import kr.co.pawong.pwbe.adoption.application.service.dto.request.AdoptionSearchCondition;
 import kr.co.pawong.pwbe.adoption.application.service.port.AdoptionSearchRepository;
 import kr.co.pawong.pwbe.adoption.infrastructure.repository.document.AdoptionDocument;
+import kr.co.pawong.pwbe.adoption.infrastructure.repository.document.AutocompleteDocument;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.client.elc.NativeQuery;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 @Repository
 @RequiredArgsConstructor
@@ -108,5 +111,28 @@ public class AdoptionSearchRepositoryImpl implements AdoptionSearchRepository {
         }
 
         return semantic.build()._toQuery();
+    }
+
+    @Override
+    public List<String> autocomplete(String keyword) {
+        var edgeQuery = NativeQuery.builder()
+                .withQuery(q -> q.match(m -> m.field("word.edge").query(keyword)))
+                .withPageable(PageRequest.of(0, 1000))
+                .build();
+        var ngramQuery = NativeQuery.builder()
+                .withQuery(q -> q.bool(b -> b
+                        .must(m1 -> m1.match(m2 -> m2.field("word.ngram").query(keyword)))
+                        .mustNot(mn -> mn.match(mn2 -> mn2.field("word.edge").query(keyword)))
+                ))
+                .withPageable(PageRequest.of(0, 1000))
+                .build();
+
+        return Stream.concat(
+                        elasticsearchOperations.search(edgeQuery, AutocompleteDocument.class)
+                                .stream().map(hit -> hit.getContent().getWord()),
+                        elasticsearchOperations.search(ngramQuery, AutocompleteDocument.class)
+                                .stream().map(hit -> hit.getContent().getWord())
+                )
+                .toList();
     }
 }
