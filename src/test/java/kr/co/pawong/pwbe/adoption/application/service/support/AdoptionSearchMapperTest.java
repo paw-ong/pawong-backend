a1baf233ff3144f9f7bd2792149e346d15e44498
Sungyeon <csy357@gmail.com>
2025-05-04 21:59:33 +0900

    [feat] toRegionList 함수 테스트 코드 작성

--- src/test/java/kr/co/pawong/pwbe/adoption/application/service/support/AdoptionSearchMapperTest.java
+++ src/test/java/kr/co/pawong/pwbe/adoption/application/service/support/AdoptionSearchMapperTest.java
@@ -1,5 +1,7 @@
 package kr.co.pawong.pwbe.adoption.application.service.support;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import kr.co.pawong.pwbe.adoption.application.service.dto.request.AdoptionSearchCondition.Region;
 import org.junit.jupiter.api.Test;
@@ -50,5 +52,50 @@ class AdoptionSearchMapperTest {
     // then
     assertThat(result).isEmpty();
   }
+  @Test
+  void 다양한_지역문자열_리스트_입력() {
+    // given
+    List<String> regions = Arrays.asList("경기도 광주시","서울특별시", "인천광역시 남동구");
+    // when
+    List<Region> regionList = AdoptionSearchMapper.toRegionList(regions);
+    // then
+    assertThat(regionList).extracting(Region::getCity, Region::getDistrict)
+        .contains(
+            tuple("경기도", "광주시"),
+            tuple("서울특별시", null),
+            tuple("인천광역시", "남동구")
+        );
+  }
+
+  @Test
+  void null_입력시_Optional이_비어있어야_한다() {
+    // given
+    String blank = null;
+    // when
+    Optional<Region> result = AdoptionSearchMapper.parseRegion(blank);
+    // then
+    assertThat(result).isEmpty();
+  }
+
+  @Test
+  void null_공백_빈_문자열_입력시_빈_리스트를_반환한다() {
+    // given
+    List<String> regions = Arrays.asList(null, "", " ");
+    // when
+    List<Region> regionList = AdoptionSearchMapper.toRegionList(regions);
+    // then
+    assertThat(regionList).isEmpty();
+  }
+
+  @Test
+  void 지역_리스트가_null() {
+    // given
+    List<String> regions = null;
+    // when
+    List<Region> regionList = AdoptionSearchMapper.toRegionList(regions);
+    // then
+    assertThat(regionList).isEmpty();
+  }
+
 
 }
