97b8a62fdda3dc1612ff61588e839dbde881019b
Sungyeon <csy357@gmail.com>
2025-05-04 19:23:07 +0900

    [refactor] 인터페이스와 메소드 파라미터에 kakao 이름이 들어가지 않도록 리팩토링

--- src/main/java/kr/co/pawong/pwbe/user/application/service/AuthServiceImpl.java
+++ src/main/java/kr/co/pawong/pwbe/user/application/service/AuthServiceImpl.java
@@ -2,6 +2,7 @@ package kr.co.pawong.pwbe.user.application.service;
 
 import jakarta.transaction.Transactional;
 import kr.co.pawong.pwbe.user.application.domain.User;
+import kr.co.pawong.pwbe.user.application.domain.UserCreate;
 import kr.co.pawong.pwbe.user.application.domain.UserUpdate;
 import kr.co.pawong.pwbe.user.application.service.port.UserCommandRepository;
 import kr.co.pawong.pwbe.user.application.service.port.UserQueryRepository;
@@ -16,11 +17,20 @@ public class AuthServiceImpl implements AuthService {
   private final UserQueryRepository userQueryRepository;
   private final UserCommandRepository userCommandRepository;
 
+  @Override
+  public User createOrGetUser(UserCreate userCreate) {
+    User getUser =  userQueryRepository.findByUserSocialId(userCreate.getSocialId());
+    if(getUser == null) {
+      return userCommandRepository.save(userCreate.toDomain());
+    }
+    return getUser;
+  }
+
   @Transactional
   @Override
   public AuthResponse signUp(Long userId, UserUpdate userUpdate) {
     User pendingUser = userQueryRepository.findByUserId(userId);
-    User updatedUser = userCommandRepository.update(pendingUser.update(userUpdate));
+    User updatedUser = userCommandRepository.updateProfile(pendingUser.update(userUpdate));
     return new AuthResponse(
         updatedUser.getUserId(),
         updatedUser.getStatus()
--- src/main/java/kr/co/pawong/pwbe/user/infrastructure/security/CustomOAuth2UserService.java
+++ src/main/java/kr/co/pawong/pwbe/user/infrastructure/security/CustomOAuth2UserService.java
@@ -4,7 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 import kr.co.pawong.pwbe.user.application.domain.User;
 import kr.co.pawong.pwbe.user.infrastructure.external.dto.KakaoUserResponse;
-import kr.co.pawong.pwbe.user.presentation.controller.port.KakaoService;
+import kr.co.pawong.pwbe.user.presentation.controller.port.AuthService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
@@ -16,13 +17,10 @@ import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
-    private final KakaoService kakaoService;
-
-    public CustomOAuth2UserService(KakaoService kakaoService) {
-      this.kakaoService = kakaoService;
-    }
+    private final AuthService authService;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -32,19 +30,19 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
         // 회원정보 저장/업데이트
         KakaoUserResponse kakaoUserResponse = KakaoUserResponse.from(oAuth2User);
-        User orGetUser = kakaoService.createOrGetUser(kakaoUserResponse);
+        User user = authService.createOrGetUser(kakaoUserResponse.toUserCreate());
 
         SecurityContextHolder.getContext().setAuthentication(
             new OAuth2AuthenticationToken(oAuth2User, oAuth2User.getAuthorities(), provider));
 
         Map<String,Object> attrs = new HashMap<>(oAuth2User.getAttributes());
-        attrs.put("userId", orGetUser.getUserId());
+        attrs.put("userId", user.getUserId());
 
         return new CustomOAuth2User(
             oAuth2User.getAuthorities(),
             oAuth2User.getAttributes(),
             "id",
-            orGetUser.getUserId()
+            user.getUserId()
         );
     }
 }
