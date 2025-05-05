efb84c4f1d96d144ecd9fc3b78c251673458ae16
Yoo Hyunseok <brianyhs@naver.com>
2025-05-05 17:44:18 +0900

    [feat] FavoritesController 구현 및 api endpoint 변동

--- src/main/java/kr/co/pawong/pwbe/favorites/presentation/FavoritesController.java
+++ src/main/java/kr/co/pawong/pwbe/favorites/presentation/FavoritesController.java
@@ -2,33 +2,68 @@ package kr.co.pawong.pwbe.favorites.presentation;
 
 import kr.co.pawong.pwbe.favorites.application.service.FavoritesService;
 import kr.co.pawong.pwbe.favorites.application.service.dto.FavoritesRequest;
-import kr.co.pawong.pwbe.favorites.enums.FavoriteTargetType;
 import kr.co.pawong.pwbe.favorites.presentation.dto.response.FavoritesResponse;
-import kr.co.pawong.pwbe.user.presentation.controller.port.UserQueryService;
+import kr.co.pawong.pwbe.user.infrastructure.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
+@RequestMapping("/api/users/me/favorites")
 @RequiredArgsConstructor
-@RequestMapping("/api")
 public class FavoritesController {
+
     private final FavoritesService favoritesService;
 
     /**
-     * TODO: Spring Security 적용 후 @AuthenticationPrincipal 등으로 userId를 받도록 변경
-     * 현재는 요청에 바로 userId 받아옴
+     * 현재 로그인된 유저가 adoption 공고를 토글 방식으로 찜한다.
+     * @param adoptionId
+     * @param principal -> 인증된 사용자 정보에서 userId를 꺼낼 수 있다.
      */
-    @PostMapping("/adoptions/{adoptionId}/favorites")
-    public ResponseEntity<FavoritesResponse> likeAdoption(@PathVariable Long adoptionId,
-                                                          @RequestBody Long userId) {
+    @PostMapping("/{adoptionId}")
+    public ResponseEntity<FavoritesResponse> toggleFavorite(
+            @PathVariable Long adoptionId,
+            @AuthenticationPrincipal CustomUserDetails principal
+    ) {
+        Long userId = principal.getUserId();
         FavoritesRequest request = FavoritesRequest.builder()
                 .userId(userId)
-                .targetId(adoptionId)
-                .targetType(FavoriteTargetType.ADOPTION)
+                .adoptionId(adoptionId)
                 .build();
 
-        FavoritesResponse response = favoritesService.like(request);
+        // 호출 결과로 찜 완료: true, 찜 취소: false
+        boolean isInFavorites = favoritesService.toggleFavorite(request);
+        // 응답 DTO
+        FavoritesResponse response = FavoritesResponse.builder()
+                .isInFavorites(isInFavorites)
+                .build();
+
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 현재 로그인된 유저가 adoptionId 공고를 찜했는지 상태를 확인한다.
+     * @param adoptionId
+     * @param principal -> 인증된 사용자 정보에서 userId를 꺼낼 수 있다.
+     */
+    @GetMapping("/{adoptionId}/status")
+    public ResponseEntity<FavoritesResponse> checkFavoriteStatus(
+            @PathVariable Long adoptionId,
+            @AuthenticationPrincipal CustomUserDetails principal
+    ) {
+        Long userId = principal.getUserId();
+
+        // true: 이미 찜한 상태, false: 찜하지 않은 상태
+        boolean isInFavorites = favoritesService.checkFavoriteStatus(FavoritesRequest.builder()
+                .userId(userId)
+                .adoptionId(adoptionId)
+                .build());
+        // 응답 DTO
+        FavoritesResponse response = FavoritesResponse.builder()
+                .isInFavorites(isInFavorites)
+                .build();
         return ResponseEntity.ok(response);
     }
 }
