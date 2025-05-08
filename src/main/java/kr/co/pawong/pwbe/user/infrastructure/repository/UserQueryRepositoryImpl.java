6e8b976465cf9d414000338e39f730da4e69d674
Sungyeon <csy357@gmail.com>
2025-05-08 22:18:24 +0900

    [refactor] user가 없을 때 USER_NOT_FOUND 에러

--- src/main/java/kr/co/pawong/pwbe/user/infrastructure/repository/UserQueryRepositoryImpl.java
+++ src/main/java/kr/co/pawong/pwbe/user/infrastructure/repository/UserQueryRepositoryImpl.java
@@ -1,5 +1,7 @@
 package kr.co.pawong.pwbe.user.infrastructure.repository;
 
+import static kr.co.pawong.pwbe.global.error.errorcode.CustomErrorCode.USER_NOT_FOUND;
+import kr.co.pawong.pwbe.global.error.exception.BaseException;
 import kr.co.pawong.pwbe.user.application.domain.User;
 import kr.co.pawong.pwbe.user.application.service.port.UserQueryRepository;
 import kr.co.pawong.pwbe.user.infrastructure.repository.entity.UserEntity;
@@ -9,21 +11,22 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class UserQueryRepositoryImpl implements UserQueryRepository {
-  private final UserJpaRepository userJpaRepository;
 
-  @Override
-  public User findByUserIdOrThrow(Long userId) {
-    return userJpaRepository.findByUserId(userId)
-        .map(UserEntity::toDomain)
-        .orElseThrow(() ->
-            new IllegalArgumentException("User not found")
-        );
-  }
+    private final UserJpaRepository userJpaRepository;
 
-  @Override
-  public User findByUserSocialId(Long socialId) {
-    return userJpaRepository.findBySocialId(socialId)
-        .map(UserEntity::toDomain)
-        .orElse(null);
-  }
+    @Override
+    public User findByUserIdOrThrow(Long userId) {
+        return userJpaRepository.findByUserId(userId)
+                .map(UserEntity::toDomain)
+                .orElseThrow(() ->
+                                new BaseException(USER_NOT_FOUND)
+                );
+    }
+
+    @Override
+    public User findByUserSocialId(Long socialId) {
+        return userJpaRepository.findBySocialId(socialId)
+                .map(UserEntity::toDomain)
+                .orElse(null);
+    }
 }
